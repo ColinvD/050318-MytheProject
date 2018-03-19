86d0ccea93b348dced500588aefc40a21c70c454
ColinvD <22950@ma-web.nl>
2018-03-19 08:52:27 +0100

    Timer, diffrent versions of patience added

---
+++ Mythe Files/Assets/Scripts/UI/Patience/VersionBar/ChangeBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeBar : MonoBehaviour {
+
+    [SerializeField]
+    private RectTransform fill;
+    [SerializeField]
+    private Slider slider;
+
+    public void ChangeBarWidth()
+    {
+        if (slider.value == 0)
+        {
+            fill.rect.Set(fill.rect.x, fill.rect.y, 0, fill.rect.height);
+        }
+        else
+        {
+            fill.rect.Set(fill.rect.x, fill.rect.y, 20, fill.rect.height);
+        }
+    }
+
+}
